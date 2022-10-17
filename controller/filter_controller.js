6d9ee4e11901ca52d96d2127055a43a581bbac8e
GovindNiranjanNarwade <govindpurva3@gmail.com>
2022-10-17 12:49:38 +0530

    address Details

--- controller/filter_controller.js
+++ controller/filter_controller.js
@@ -1,12 +1,21 @@
 const product = require("../model/Product_mode")
 exports.getProductbyPriceRange = async(req,res) =>{
 try {
-    const  result = await product.find({ProductPrize:{$lte:5000}} && {ProductPrize:{$lte:1000}} )
-    res.json({
-        success:true,
-        message:"get Product by Price Range",
-        data : result
-    })
+    const  result = await product.find({ProductPrize:{$lte:req.body.Price1} }  )
+    const  result1 = await product.find({ProductPrize:{$gte:req.body.Price2}}  )
+    if((result && result1 )){
+        res.json({
+            success:true,
+            message:"get Product by Price Range",
+            data : result
+        })
+    }
+    else{
+        res.json({
+            message :"Please provide Correct Price Range "
+        })
+    }
+    
 } catch (error) {
     res.json({
         success:false,
--- routes/filter_routes.js
+++ routes/filter_routes.js
@@ -5,42 +5,48 @@ const router = express.Router()
  * @swagger
  * components:
  *   schemas:
- *     admin:
+ *     priceRange:
  *       type: object
  *       required:
- *         - Email
- *         - Password
+ *         - Price1
+ *         - Price2
  *       properties:
- *         Email:
- *           type: string
- *           description: admin@gmail.com
- *         Password:
- *           type: string
- *           description: password
+ *         Price1:
+ *           type: integer
+ *           description: Price1
+ *         Price2:
+ *           type: integer
+ *           description: Price2
  *       example:
- *         Email: admin@gmail.com
- *         Password: 12345
+ *         Price1: lessThanOnePrice
+ *         Price2: greaterThanOnePrice
  *        
  *
  */
 
 /**
  * @swagger
- * /api/v1/admin/getAdmin:
- *   get:
- *     summary: get all admin
- *     tags: [admin]
+ * /api/v1/getProductbyPriceRange:
+ *   post:
+ *     summary: get Product by PriceRange
+ *     tags: [priceRange]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/priceRange'
  *     responses:
  *       200:
- *         description: get All admin
+ *         description: get Product by PriceRange successfull
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               items:
- *                 $ref: '#/components/schemas/admin'
+ *               $ref: '#/components/schemas/priceRange'
+ *       500:
+ *         description: Some server error
  */
-router.route("/getProductbyPriceRange").get(getProductbyPriceRange)
+router.route("/getProductbyPriceRange").post(getProductbyPriceRange)
 
 
 module.exports = router
